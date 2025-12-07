ca33a36ce9857203c2dce2742fcba8463078b904
Pedro Moraes <pedro693.ph@gmail.com>
2025-12-06 23:03:57 -0300

    feat(agendamento): criando funcionanlidade e tela de agendamento de servicos

--- app/Model/Agendamento.php
+++ app/Model/Agendamento.php
@@ -1,7 +1,8 @@
 <?php
 App::uses('AppModel', 'Model');
 
-class Agendamento extends AppModel {
+class Agendamento extends AppModel
+{
 
     public $displayField = 'nome_cliente';
 
@@ -23,14 +24,19 @@ class Agendamento extends AppModel {
                 'message' => 'Por favor, insira uma data e hora válidas no formato DD/MM/AAAA HH:MM.',
                 'allowEmpty' => false
             )
-			),
-		'data_hora_fim' => array(
-			'custom_format' => array(
-				'rule' => array('validateDateTimeFormat'),
-				'message' => 'Por favor, insira uma data e hora válidas (DD/MM/AAAA HH:MM).',
-				'allowEmpty' => true // Pode ser vazio? Se sim, 'true'. Se não, 'false'.
-			)
-    )
+        ),
+        'data_hora_fim' => array(
+            'custom_format' => array(
+                'rule' => array('validateDateTimeFormat'),
+                'message' => 'Por favor, insira uma data e hora válidas (DD/MM/AAAA HH:MM).',
+                'allowEmpty' => true
+            ),
+            'isAfterOrEqualStart' => array(
+                'rule'    => array('checkEndAfterOrEqualStart'),
+                'message' => 'A data final não pode ser menor que a data inicial.',
+                'allowEmpty' => true
+            )
+            ),
     );
 
     public $belongsTo = array(
@@ -40,7 +46,8 @@ class Agendamento extends AppModel {
     );
 
     // ### PARTE 2: ADICIONAR A NOSSA FUNÇÃO DE VALIDAÇÃO ###
-    public function validateDateTimeFormat($check) {
+    public function validateDateTimeFormat($check)
+    {
         // Pega o valor do campo (ex: '04/12/2025 15:30')
         $dateTimeString = array_values($check)[0];
 
@@ -54,26 +61,53 @@ class Agendamento extends AppModel {
         return $d && $d->format('d/m/Y H:i') === $dateTimeString;
     }
 
-	public function beforeSave($options = array()) {
-		// Lista de campos que precisam de conversão de formato
-		$camposDeData = array('data_hora_inicio', 'data_hora_fim');
+    public function beforeSave($options = array())
+    {
+        // Lista de campos que precisam de conversão de formato
+        $camposDeData = array('data_hora_inicio', 'data_hora_fim');
 
-		foreach ($camposDeData as $campo) {
-			if (!empty($this->data[$this->alias][$campo])) {
-				$dataHoraDoFormulario = $this->data[$this->alias][$campo];
-				
-				// Só converte se não estiver no formato do banco
-				// (evita reconverter ao editar sem mudar a data)
-				if (strpos($dataHoraDoFormulario, '/') !== false) {
-					$dataObj = DateTime::createFromFormat('d/m/Y H:i', $dataHoraDoFormulario);
-					if ($dataObj) {
-						$this->data[$this->alias][$campo] = $dataObj->format('Y-m-d H:i:s');
-					} else {
-						return false; // Formato inválido
-					}
-				}
-			}
-		}
-		return true;
-	}
+        foreach ($camposDeData as $campo) {
+            if (!empty($this->data[$this->alias][$campo])) {
+                $dataHoraDoFormulario = $this->data[$this->alias][$campo];
+
+                // Só converte se não estiver no formato do banco
+                // (evita reconverter ao editar sem mudar a data)
+                if (strpos($dataHoraDoFormulario, '/') !== false) {
+                    $dataObj = DateTime::createFromFormat('d/m/Y H:i', $dataHoraDoFormulario);
+                    if ($dataObj) {
+                        $this->data[$this->alias][$campo] = $dataObj->format('Y-m-d H:i:s');
+                    } else {
+                        return false; // Formato inválido
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public function checkEndAfterOrEqualStart($check)
+    {
+        // Se fim vazio, ok
+        $fim = array_values($check)[0];
+
+        if (empty($this->data[$this->alias]['data_hora_inicio']) || empty($fim)) {
+            return true;
+        }
+
+        // Converta ambos para timestamp (considerando que foi convertido para Y-m-d H:i:s no beforeSave)
+        $inicio = $this->data[$this->alias]['data_hora_inicio'];
+        $fim    = $this->data[$this->alias]['data_hora_fim'];
+
+        // Se algum vier no formato brasileiro, converta:
+        if (strpos($inicio, '/') !== false) {
+            $dt = DateTime::createFromFormat('d/m/Y H:i', $inicio);
+            if ($dt) $inicio = $dt->format('Y-m-d H:i:s');
+        }
+        if (strpos($fim, '/') !== false) {
+            $dt = DateTime::createFromFormat('d/m/Y H:i', $fim);
+            if ($dt) $fim = $dt->format('Y-m-d H:i:s');
+        }
+
+        return strtotime($fim) >= strtotime($inicio);
+    }
 }
